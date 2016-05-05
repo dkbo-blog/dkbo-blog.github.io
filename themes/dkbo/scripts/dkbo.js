b27da8d892bc3fd07578be76f56e9a4e19833cf7
DKBO <dk880842@gmail.com>
2016-05-05 22:01:43 +0800

    upload

--- themes/dkbo/scripts/dkbo.js
+++ themes/dkbo/scripts/dkbo.js
@@ -26,9 +26,8 @@ var init = {
     },
     feature: function(post) {
         var matches;
-        if (post.content) {
+        if (post.content)
             matches = post.content.match('https://www.youtube.com/embed/([^"&?/ ]{11})');
-        }
         if (post.feature) {
             return "/" + post.feature;
         } else if (matches) {
@@ -37,19 +36,7 @@ var init = {
             return "/images/feature/dkbo.jpg";
         }
     },
-    // filter: function(arg) {
-    //   var object = [];
-    //   if(arg.type === 'tags') {
-    //       return arg;
-    //   } else if(arg.type === 'category') {
-    //     var o = arg.post.map(function(x){
-    //       if(x.categories.data.indexOf(arg.filter) == -1){
-    //         object.push(x);
-    //       }
-    //     });
-    //     return JSON.parse(object);
-    //   }
-    // },
+
 };
 
 // Category
@@ -57,7 +44,6 @@ hexo.extend.helper.register('cName', init.category.name);
 
 hexo.extend.helper.register('cPath', init.category.path);
 
-// hexo.extend.helper.register('filter', init.filter);
 
 // feature
 hexo.extend.helper.register('feature', init.feature);
